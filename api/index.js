c6d4f7fb454f2903a199e118fdf5c34b26df75cc
notkaswan <deepak.kaswan.1999@gmail.com>
2023-05-10 13:34:34 +0530

    modified:   api/index.js

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 app.use('/uploads', express.static(__dirname+'/uploads'))
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5174',
+    origin: 'http://localhost:5173',
 }));
 
 mongoose.connect(process.env.MONGO_URL)
