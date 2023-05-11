8798fc1e1ec6c599cb6f1edd34adf7806bab86ca
notkaswan <deepak.kaswan.1999@gmail.com>
2023-05-11 16:12:03 +0530

    modified:   api/index.js 	modified:   client/src/Layout.jsx 	modified:   client/src/PhotosUploader.jsx 	modified:   client/src/pages/IndexPage.jsx 	modified:   client/src/pages/PlacesPage.jsx

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,8 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function IndexPage() {
+  const [places, setPlaces] = useState([])
+  useEffect(() => {
+    axios.get('/places').then(response => {
+      setPlaces(response.data)
+    })
+  }, [])
     return (
-      <div>
-        index page
+      <div className="mt-8 grid gap-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {places.length > 0 && places.map(place => (
+          <div>
+            <div className="bg-gray-500 rounded-2xl flex">
+              {place.photos?.[0] && (
+                <img className="rounded-2xl object-cover aspect-square" src={'http://localhost:4000/uploads/'+place.photos?.[0]} alt />
+              )}
+            </div>
+            <h2 className="text-sm truncate">{place.title}</h2>
+            <h3 className="font-bold">{place.address}</h3>
+          </div>
+        ))}
       </div>
     );
 }
