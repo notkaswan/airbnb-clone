8e7ffd8c238ef36e967ef828a0d8b59b958f0d50
notkaswan <deepak.kaswan.1999@gmail.com>
2023-05-12 16:41:55 +0530

    modified:   client/src/pages/PlacesPage.jsx

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -24,15 +24,15 @@ export default function PlacesPage() {
                         Add new place
                     </Link>
                 </div>
-                <div>
+                <div className="mt-4">
                     {places.length > 0 && places.map(place => (
-                        <Link to={'/account/places/'+place._id} className="flex cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl">
-                            <div className="flex w-32 h-32 bg-gray-300">
+                        <Link to={'/account/places/'+place._id} className="flex cursor-pointer gap-4 mb-2 bg-gray-100 p-4 rounded-2xl">
+                            <div className="flex w-32 h-32 bg-gray-300 grow shrink-0">
                                 {place.photos.length > 0 && (
                                     <img className="object-cover" src={'http://localhost:4000/uploads/'+place.photos[0]} alt='' />
                                 )}
                             </div>
-                            <div>
+                            <div className="grow-0 shrink">
                                 <h2 className="text-xl ">{place.title}</h2>
                                 <p className="text-sm mt-2">{place.description}</p>
                             </div>
