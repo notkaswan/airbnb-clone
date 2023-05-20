32d2835c8999139121f35060338d891228b80b6f
notkaswan <deepak.kaswan.1999@gmail.com>
2023-05-20 20:24:14 +0530

    modified:   client/src/pages/PlacePage.jsx

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -1,7 +1,18 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 
 export default function PlacePage() {
     const {id} = useParams();
+    const [place, setPlace] = useState(null);
+    useEffect(() => {
+      if(!id) {
+        return
+      }
+      axios.get(`/place/${id}`).then(response => {
+        setPlace(response.data)
+      })
+    }, [id])
   return (
     <div>
         place page: {id}
