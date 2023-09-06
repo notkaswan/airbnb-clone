6b6141c7e3a497a516906ffb0615106d87b08117
Deepak Kaswan <62922598+notkaswan@users.noreply.github.com>
2023-09-06 20:11:43 +0530

    modified:   api/index.js 	modified:   client/src/UserContext.jsx

--- api/index.js
+++ api/index.js
@@ -19,13 +19,12 @@ const jwtSecret = "asdfe2fsdf8dafdf7dfsdf23q4v5aw";
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
-app.use(cors());
-
-// {
-// credentials: true,
-// origin: [BASE_URL, "https://rent-a-place-r1xa.onrender.com"],
-// "Access-Control-Allow-Origin": "*",
-// }
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
 
 mongoose.connect(process.env.MONGO_URL);
 
